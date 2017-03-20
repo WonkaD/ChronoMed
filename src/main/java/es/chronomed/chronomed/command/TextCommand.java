f20a5e9a88a2c0e66694b7dff04b6c46ca0ded02
Entrar <Entrar>
2017-03-20 08:53:30 +0000

    Primer commit

---
+++ src/main/java/es/chronomed/chronomed/command/TextCommand.java
@@ -0,0 +1,22 @@
+package es.chronomed.chronomed.command;
+
+import es.chronomed.chronomed.beans.State1;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TextCommand extends FrontCommand {
+    
+    @Override
+    public void process() {
+        State1 s;
+        try {
+            s = InitialContext.doLookup("java:global/ChronoMed/State1");
+            request.setAttribute("mensaje", s.getString());
+        } catch (NamingException ex) {
+            Logger.getLogger(TextCommand.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        forward("/index.jsp");
+    }
+}
